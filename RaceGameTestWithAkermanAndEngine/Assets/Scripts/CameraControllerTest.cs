f88d31718b8f9582d1c1d81ec156653db41b055e
AlekseyBrovko <96891019+AlekseyBrovko@users.noreply.github.com>
2022-08-26 00:25:21 +0300

    --

--- RaceGameTestWithAkermanAndEngine/Assets/Scripts/CameraControllerTest.cs
+++ RaceGameTestWithAkermanAndEngine/Assets/Scripts/CameraControllerTest.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class CameraControllerTest : MonoBehaviour
 {
     private GameObject Player;
     private GameObject cameraFollow;
